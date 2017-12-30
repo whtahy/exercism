69216d1c512f8b88f87370de16405ae5db155134
Tae Hyon Whang <whtahy@users.noreply.github.com>
2017-12-29 23:29:40 -0500

    Lint: haskell 3

--- haskell/03 IsbnVerifier.hs
+++ haskell/03 IsbnVerifier.hs
@@ -9,13 +9,13 @@ isbn s
   where
     x     = parse s
     coeff = [10, 9 .. 1]
-    valid = length x == 10 && not (elem 10 $ init x)
+    valid = length x == 10 && notElem 10 (init x)
 
 parse :: String -> [Int]
 parse [] = []
 parse (x:xs)
-    | elem x ['0' .. '9'] = [read [x]] ++ parse xs
-    | elem x ['X']        = [10] ++ parse xs
+    | elem x ['0' .. '9'] = read [x] : parse xs
+    | elem x ['X']        = 10 : parse xs
     | otherwise           = parse xs
 
 dotProd :: Num a => [a] -> [a] -> a
