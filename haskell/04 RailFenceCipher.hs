588070462a41fa347333336f3fd2b35afb88e32a
Tae Hyon Whang <whtahy@users.noreply.github.com>
2018-01-05 20:44:18 -0500

    Refactor: haskell 4

--- haskell/04 RailFenceCipher.hs
+++ haskell/04 RailFenceCipher.hs
@@ -6,19 +6,21 @@ module RailFenceCipher
   , decode
   ) where
 
+import Data.List (sortOn)
+
 encode :: Int -> [a] -> [a]
-encode n s = concat $ sort ii ss
+encode n s = f ss
   where
     ss = zip p s
     p  = cycle $ ii ++ reverse (drop 1 $ init ii)
     ii = [1 .. n]
 
 decode :: Int -> [a] -> [a]
-decode n s = concat $ sort ii ss
+decode n s = f ss
   where
     ss = zip p s
     p  = encode n ii
     ii = [1 .. length s]
 
-sort :: [Int] -> [(Int, t)] -> [[t]]
-sort ii ss = [[ snd x | x <- ss, fst x == i] | i <- ii]
+f :: Ord a => [(a, b)] -> [b]
+f x = map snd $ sortOn fst x
