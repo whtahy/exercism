abdaa04a260bf68d2562e1e0d6b842a66b04cf9c
Tae Hyon Whang <whtahy@users.noreply.github.com>
2018-01-06 18:18:19 -0500

    Refactor: haskell 8

--- haskell/08 RunLength.hs
+++ haskell/08 RunLength.hs
@@ -3,37 +3,24 @@ module RunLength
   , encode
   ) where
 
-encode :: String -> String
-encode s = concat $ zipWith (++) a b
-  where
-    ss = split s
-    a  = map (f . show . length) ss
-    b  = split $ map head ss
-    f x
-        | x == "1"  = ""
-        | otherwise = x
-
-split :: String -> [String]
-split [] = []
-split s  = fst x : split (snd x)
-  where
-    x = span (== head s) s
+import Data.Char (isDigit)
+import Data.List (group, groupBy)
 
 decode :: String -> String
-decode = make . parse
-
-make :: [(Int, Char)] -> String
-make []     = []
-make (x:xs) = uncurry replicate x ++ make xs
-
-parse :: String -> [(Int, Char)]
-parse [] = []
-parse s  = (a, b) : parse xs
+decode = f . g
   where
-    x  = span (flip elem ['0' .. '9']) s
-    a  = if null (fst x) then
-            1
-         else
-            read $ fst x
-    b  = head $ snd x
-    xs = tail $ snd x
+    g = groupBy (\x y -> all isDigit [x,y])
+
+    f []  = []
+    f [x] = x
+    f (x:y:zs)
+        | isDigit (head x) = replicate (read x) (head y) ++ f zs
+        | otherwise        = head x : f (y:zs)
+
+-- Credit to https://github.com/cjlarose
+-- Adapted from https://git.io/vNThm
+encode :: String -> String
+encode s = concatMap f $ group s
+  where
+    f [x] = [x]
+    f x   = (show . length) x ++ take 1 x
