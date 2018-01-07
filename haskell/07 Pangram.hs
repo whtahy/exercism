86438054886968c20aec49dd731b31296f206aab
Tae Hyon Whang <whtahy@users.noreply.github.com>
2018-01-07 00:11:24 -0500

    Refactor: haskell 7

--- haskell/07 Pangram.hs
+++ haskell/07 Pangram.hs
@@ -5,4 +5,6 @@ module Pangram
 import Data.Char (toLower)
 
 isPangram :: String -> Bool
-isPangram s = and [elem i (map toLower s) | i <- ['a' .. 'z']]
+isPangram s = all (flip elem x) ['a' .. 'z']
+  where
+    x = map toLower s
