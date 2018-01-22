cc2362b5c93fa72794fbe3f19cc304ac14c7d53d
Tae Hyon Whang <whtahy@users.noreply.github.com>
2018-01-22 00:27:26 -0500

    Init: haskell 39 (Roman.hs)

---
+++ haskell/39 Roman.hs
@@ -0,0 +1,30 @@
+module Roman (numerals) where
+
+numerals :: Integer -> Maybe String
+numerals n
+    | n <= 0    = Nothing
+    | otherwise = Just . concat $ f n dict
+
+f :: Integer -> [(Integer, String)] -> [String]
+f n [] = []
+f n ((i,s):xs) = replicate r s ++ f n' xs
+  where
+    r  = fromIntegral $ div n i
+    n' = rem n i
+
+dict =
+    reverse
+    [ (   1, "I")
+    , (   4, "IV")
+    , (   5, "V")
+    , (   9, "IX")
+    , (  10, "X")
+    , (  40, "XL")
+    , (  50, "L")
+    , (  90, "XC")
+    , ( 100, "C")
+    , ( 400, "CD")
+    , ( 500, "D")
+    , ( 900, "CM")
+    , (1000, "M")
+    ]
